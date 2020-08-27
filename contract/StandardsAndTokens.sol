33f7f5168daf82c3fe78505cbed59dbbcbc3240a
Dexaran <dexaran820@gmail.com>
2020-08-27 17:40:27 +0400

    Update StandardsAndTokens.sol

--- contract/StandardsAndTokens.sol
+++ contract/StandardsAndTokens.sol
@@ -1,1 +1,74 @@
 
+pragma solidity ^0.4.25;
+ 
+ contract StandardizedToken {
+     string public standard;
+     mapping (address => uint256) public balances;
+     
+     function askStandard() public view returns (string)
+     {
+         return standard;
+     }
+     
+     function balanceOf(address _who) public view returns (uint256)
+     {
+         return balances[_who];
+     }
+ }
+ 
+ 
+ contract ERC20Token is StandardizedToken{
+     constructor() public
+     {
+         standard = "ERC20";
+     }
+     
+     function transfer(address _to, uint _amount) public
+     {
+         assert(balances[msg.sender] >= _amount);
+         balances[msg.sender] -= _amount;
+         balances[_to]        += _amount;
+     }
+ }
+ 
+ 
+ contract ERC223Token is StandardizedToken{
+     constructor() public
+     {
+         standard = "ERC223";
+     }
+     
+     event LogData(bytes);
+     
+     function transfer(address _to, uint _amount, bytes memory _data) public
+     {
+         assert(balances[msg.sender] >= _amount);
+         balances[msg.sender] -= _amount;
+         balances[_to]        += _amount;
+         
+         emit LogData(_data);
+     }
+ }
+ 
+ 
+ contract CustomToken is StandardizedToken{
+     constructor() public
+     {
+         standard = "Custom";
+     }
+     
+     function sendToken(address _to, uint _amount) public
+     {
+         uint256 _balance_before = balances[msg.sender];
+         
+         assert(balances[msg.sender] >= _amount);
+         balances[msg.sender] -= _amount;
+         balances[_to]        += _amount;
+         
+         if(_balance_before < balances[msg.sender])
+         {
+             revert();
+         }
+     }
+ }
+ 
